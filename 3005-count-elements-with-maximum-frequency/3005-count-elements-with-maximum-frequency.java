a84b4958007e71db1fcb02b5f51186ad4e9278c3
VANSHIKA SINGH <32262806+vanshikasingh0065@users.noreply.github.com>
2024-01-28 18:36:40 -0500

    Time: 3 ms (31.02%), Space: 41.9 MB (95.69%) - LeetHub

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.java
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.java
@@ -9,14 +9,15 @@ class Solution {
             if(map.containsKey(i)){
                 int k = map.get(i);
                 map.replace(i, k+1);
+                max = Math.max(max,k+1); 
             }else{
                 map.put(i,1);
             }
         }
      //   1,1,1,7,7,7,9,9,9,9,8,8,8,8
-        for(Map.Entry<Integer,Integer> m: map.entrySet()){
-          max = Math.max(max,m.getValue());
-        }
+        // for(Map.Entry<Integer,Integer> m: map.entrySet()){
+        //   max = Math.max(max,m.getValue());
+        // }
         for(Map.Entry<Integer,Integer> p: map.entrySet()){
           if(p.getValue()==max){
               count++;
