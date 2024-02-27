03ab4ab34662ec791e9e18d9c80beb986250830c
VANSHIKA SINGH <32262806+vanshikasingh0065@users.noreply.github.com>
2024-02-27 12:58:36 -0500

    Time: 7 ms (6.59%), Space: 44 MB (5.09%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,13 +1,44 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        Arrays.sort(nums);
-        
-        for(int i=0; i<nums.length; i++){
-            if(nums[i]==target){
+        // Arrays.sort(nums);
+
+        // for(int i=0; i<nums.length; i++){
+        // if(nums[i]==target){
+        // return true;
+        // }
+        // }
+
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+
+            int mid = (low + high) / 2;
+            System.out.println(nums[mid]);
+            if (nums[mid] == target) {
                 return true;
             }
+            // We know that right side is for sure sorted or left side is unsorted
+            if (nums[mid] < nums[high] || nums[mid] < nums[low]) {
+                if (target > nums[mid] && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+            // We know that left side is for sure sorted or right side is unsorted
+            else if (nums[mid] > nums[low] || nums[mid] > nums[high]) {
+                if (nums[mid] > target && target >= nums[low]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+
+            } else {
+                low++;
+            }
         }
-        
+
         return false;
     }
 }
