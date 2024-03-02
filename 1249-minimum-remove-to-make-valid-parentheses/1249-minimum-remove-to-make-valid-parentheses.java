1dffe3db1ac741e5583be6cd37fa98e40240ef6e
VANSHIKA SINGH <32262806+vanshikasingh0065@users.noreply.github.com>
2024-03-02 01:04:49 -0500

    Time: 23 ms (47.72%), Space: 45.3 MB (75.02%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        //Initialaizing a stack
+        
+        Stack<Integer> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder(s);
+        
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)=='('){
+                stack.push(i);
+            }else if(s.charAt(i) ==')'){
+                if(!stack.isEmpty() && s.charAt(stack.peek())=='('){
+                    stack.pop();
+                }else{
+                    stack.push(i);
+                }
+            }
+        }
+        
+         while(!stack.isEmpty()){
+            sb.deleteCharAt(stack.pop());
+        }
+        return sb.toString();
+        
+    }
+}
