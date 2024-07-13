e07ff36fdcd36c62488ef543d6ac6d06f20e5030
VANSHIKA SINGH <32262806+vanshikasingh0065@users.noreply.github.com>
2024-07-12 23:37:23 -0400

    Time: 854 ms (5.22%), Space: 60.2 MB (5.06%) - LeetHub

--- 3207-maximum-points-after-enemy-battles/3207-maximum-points-after-enemy-battles.java
+++ 3207-maximum-points-after-enemy-battles/3207-maximum-points-after-enemy-battles.java
@@ -3,11 +3,17 @@ class Solution {
         Arrays.sort(enemyEnergies);
         if (currentEnergy < enemyEnergies[0])
             return 0;
-
+// 2,2,3
+        //2
+    //    5, - 7- 9
+        
         long totalEnergy = currentEnergy;
-        for (int j = enemyEnergies.length - 1; j > 0; --j)
+        for (int j = enemyEnergies.length - 1; j > 0; --j){
+             totalEnergy = totalEnergy + enemyEnergies[j];
+             System.out.println(totalEnergy);
+        }
 
-            totalEnergy = totalEnergy + enemyEnergies[j];
+           
 
         return totalEnergy / enemyEnergies[0];
     }
