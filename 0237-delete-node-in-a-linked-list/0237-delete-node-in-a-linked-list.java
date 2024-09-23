f470eca031781d339d2e0187ac94583cd5d0f256
VANSHIKA SINGH <32262806+vanshikasingh0065@users.noreply.github.com>
2024-09-23 15:17:36 -0400

    Time: 0 ms (100.00%), Space: 44.4 MB (6.03%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
@@ -8,10 +8,16 @@
  */
 class Solution {
     public void deleteNode(ListNode node) {
-        if(node!=null && node.next!=null){
-            node.val = node.next.val;
-            node.next = node.next.next;
-        }
+        //we copy the value of the next node into the current node
+        //We adjust the pointer to skip the next node 
+        
+       // for example 
+        // I had 6, 8, 9, 0
+        // remover 8
+       // so 9 comes forward and pointer connecting 8 and 9 now connects 9 and 0 since 9 came forward
+        
+        node.val = node.next.val;
+        node.next = node.next.next;
         
     }
 }
