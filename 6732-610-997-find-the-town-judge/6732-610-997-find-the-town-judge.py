54ca66927d7ed4342a62d0d72fc62b21ae521621
VANSHIKA SINGH <32262806+vanshikasingh0065@users.noreply.github.com>
2025-03-02 05:06:36 +0530

    Time: 12 ms, Memory: 21.1 MB - LeetHub

--- 6732-610-997-find-the-town-judge/6732-610-997-find-the-town-judge.py
+++ 6732-610-997-find-the-town-judge/6732-610-997-find-the-town-judge.py
@@ -1,7 +1,7 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
 
-        adj = [[] for _ in range(n+1)]
+        adj = [[] for _ in range(n+1)] # Fix: Increase size to n+1
         indegree= [0]*(n+1)
 
         for c1,c2 in trust:
@@ -9,7 +9,7 @@ class Solution:
             indegree[c2]+=1
         
         for i in range(1,n+1):
-            if indegree[i] == n - 1 and not adj[i]:
+            if indegree[i] == n - 1 and not adj[i]:  # Trusted by n-1 people and trusts nobody
                 return i
 
 
