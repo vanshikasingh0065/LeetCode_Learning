6f6c3386ca2c849be08aac3351abc367b652b03c
VANSHIKA SINGH <32262806+vanshikasingh0065@users.noreply.github.com>
2025-03-01 05:51:20 +0530

    Time: 0 ms, Memory: 18.3 MB - LeetHub

--- 8615-395-785-is-graph-bipartite/8615-395-785-is-graph-bipartite.py
+++ 8615-395-785-is-graph-bipartite/8615-395-785-is-graph-bipartite.py
@@ -1,27 +1,32 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        #we are given adj list
-        #creating a color arrat
-        n = len(graph)
-        color = [-1]*n
-
+        n= len(graph)
+        color= [-1]*n
         for i in range(n):
-            if color[i]==-1:
-                if(self.dfs(i,0, graph,color)==False):
+            if(color[i]==-1):
+                if(self.check(i,graph,color)==False):
                     return False
 
         return True
 
-    def dfs(self,node,col, graph, color):
-        color[node]=col
-        for b in graph[node]:
-            if color[b]==-1:
-                if(self.dfs(b,not col, graph, color)==False):
-                    return False
+
+    def check(self,start,graph, color):
+        queue= deque()
+        queue.append(start)
+        color[start]=0
+
+        while queue:
+            node = queue.popleft()
+            col = color[node]
+            for k in graph[node]:
+                
+                if(color[k]==-1):
+                    color[k]= 1- col
+                    queue.append(k)
+                else:
+                    if(color[k]==col):
+                        return False
                     
-            else:
-                if color[b]==col:
-                    return False
+        return True
 
-
-
+        
